716a4f9d55d887bad5aa3d077bbd827ce1785b76
bzapf <{69807673}+{bzapf}@users.noreply.github.com>
2024-01-26 16:48:26 +0100

    dockerfile for python app

---
+++ app/main.py
@@ -0,0 +1,23 @@
+# create a fastapi app
+# the app should respond with 'Hello World' when a GET request is made
+
+import uvicorn
+from fastapi import FastAPI
+app = FastAPI()
+
+
+@app.get("/")
+def hello_world():
+    return "Hello Sebas"
+
+# run the app with uvicorn
+# uvicorn app:app --reload
+# --reload flag will reload the server when changes are made to the code
+
+# open the app in the browser
+# use python api of uvicorn to open the app in the browser
+
+
+if __name__ == "__main__":
+
+    uvicorn.run("app:app", reload=True, host="localhost", port=8000)
